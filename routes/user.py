a5fa60a31641722f1cdec98b22de8ee952f5e1a4
Marco Joaquin <marquitoing22@gmail.com>
2026-02-22 22:08:33 -0500

    Add put delete and getone

--- routes/user.py
+++ routes/user.py
@@ -2,12 +2,17 @@ from fastapi import APIRouter
 from models.user import User
 from config.db import conn
 from schemas.user import userEntity ,usersEntity
+from bson import objectId
 
 user = APIRouter()
 
 @user.get('/')
 async def find_all_users():
    return usersEntity(conn.proyectodb.users.find())
+
+@user.get('/{id}')
+async def find_one_user(id,user :User) :
+   return userEntity(conn.proyectodb.users.find_one({"_id":objectId(id)}))
 
 @user.post('/')
 async def create_user(user : User):
@@ -20,5 +25,12 @@ async def create_user(user : User):
     return userEntity(usuario_encontrado)
 
 
-
-
+@user.put('/{id}')
+async def update_user(id,user:User):
+   conn.proyectodb.users.find_one_and_update({"_id": objectId(id)} ,{"$set":dict(user)})
+   return userEntity(conn.proyectodb.users.find_one({"_id":objectId(id)})) 
+  
+@user.delete('/{id}')
+async def delete_user(id,user:User):
+   
+   return userEntity(conn.proyectodb.users.find_one_and_delete({"_id":objectId(id)})) 
