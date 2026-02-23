94c355513ea4191cd951e01e26ca3313525a5ffa
Marco Joaquin <marquitoing22@gmail.com>
2026-02-22 21:51:47 -0500

    Get and Post funcionales

--- routes/user.py
+++ routes/user.py
@@ -1,11 +1,24 @@
 from fastapi import APIRouter 
 from models.user import User
 from config.db import conn
+from schemas.user import userEntity ,usersEntity
 
 user = APIRouter()
 
 @user.get('/')
 async def find_all_users():
-    return conn.local.user.find()
+   return usersEntity(conn.proyectodb.users.find())
+
+@user.post('/')
+async def create_user(user : User):
+    nuevo_usuario = dict(user)
+    # Insertar en la nueva colección
+    id_insertado = conn.proyectodb.users.insert_one(nuevo_usuario).inserted_id
+    
+    # Buscar el documento único creado usando el ID
+    usuario_encontrado = conn.proyectodb.users.find_one({"_id": id_insertado})
+    return userEntity(usuario_encontrado)
+
+
 
 
--- schemas/user.py
+++ schemas/user.py
@@ -1,6 +1,11 @@
-def userEntity(item) -> dict 
+def userEntity(item) -> dict :
     return { 
      
-      "id"
-
+      "id":str(item["_id"]),
+      "name":item["name"],
+      "email":item["email"],
+      "password":item["password"]
     }
+
+def usersEntity(entity) -> list: 
+    return [userEntity(item) for item in entity]
